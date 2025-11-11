8b2f2c81443f9c5716a778994a7263972a6f1b0a
Antoine Morvan <antoine.morvan.phd@gmail.com>
2025-11-11 18:14:11 +0100

    Add main setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+SETUP_SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE))
+
 (
     sudo dnf update -y
     sudo dnf install -y \
         gcc automake autoconf libtool pkgconfig \
-        cmake vim htop btop rsync geany git terminator firefox chromium byobu
+        cmake vim htop btop rsync geany git terminator firefox chromium byobu \
+        curl wget epel-release
 )
+
+${SETUP_SCRIPT_DIR}/modulefiles/setup.sh
+${SETUP_SCRIPT_DIR}/bashrc/setup.sh
 
 
 exit 0
